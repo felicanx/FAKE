0b9c93c5dd758cc11c3bedc4b17823cfcfce167e
felicanx <96733551+felicanx@users.noreply.github.com>
2022-04-04 22:46:55 +0530

    Update fake.py

--- fake.py
+++ fake.py
@@ -2,20 +2,38 @@
 import argparse
 import os
 import requests
+from termcolor import colored 
 import subprocess
 
-print("+++++++++++++++++++++++++++++++++")
-print("+ FAKE is a easy recon tool +++++")
-print("+++++++++++++++++++++++++++++++++")
-print("Automation In Progress WAIT!!!!!!")
 
 parser = argparse.ArgumentParser(description="FAKE is a recon automation tool for a particular domain you can use this as TYPE as  [ python3 fake.py -d domain.com ]")
 parser.add_argument("-d",type=str,help="Domain",required=True)
 
 x=parser.parse_args()
 
-os.system('touch subdomain.txt')
 
+
+print(colored(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++++","red",attrs=['bold']))
+print(colored("+ _ ________      ____            _           _ _________+","blue",attrs=['bold']))
+print(colored("+| |________|    /    \          | |   / /   | |_________+","blue",attrs=['bold']))
+print(colored("+| |            / /  \_\         | |  / /    | |         +","blue",attrs=['bold']))
+print(colored("+| |------     / /    \_\        | | / /     | |         +","blue",attrs=['bold']))
+print(colored("+| |______|   / /      \_\       | |/ /      | |------   +","blue",attrs=['bold']))
+print(colored("+| |         / / ====== \_\      | |\ \      | |------   +","blue",attrs=['bold']))
+print(colored("+| |        / /          \_\     | | \ \     | |         +","blue",attrs=['bold']))
+print(colored("+| |       / /            \_\    | |  \ \    | |---------+","blue",attrs=['bold']))
+print(colored("+|_|      /_/              \_\   |_|   \ \   |_|---------+","blue",attrs=['bold']))
+print(colored(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++++","red",attrs=['bold']))
+print("\n")
+print("+++++ FAKE is a easy recon tool +++++")
+print("+++++++++++++++++++++++++++++++++")
+print(colored("Automation In Progress WAIT!!!!!!","red",attrs=['bold']))
+
+
+
+
+
+file=open('subdomain.txt','w+')
 file0=open('wordlist.txt','r')
 content = file0.read()
 
@@ -24,7 +42,7 @@ for subdomain in subdomains:
 	d1=f"http://{subdomain}.{x.d}"
 	d2=f"https://{subdomain}.{x.d}"
 
-	try:
+	try:S
 		requests.get(d1)
 		file1 = open('subdomain.txt','a')
 		file1.writelines(d1 + "\n")
@@ -39,12 +57,13 @@ for subdomain in subdomains:
 	finally:
 		file0.close()
 
-print("+++++++++++++++++++++++++++++++++")
-print("        ||        ")
-print("        VV        ")
-print('Checking status code ')
+print("\n")
+print(colored("+++++++++++++++++++++++++++++++++++++","blue",attrs=['bold']))
+print(colored("        ||     ||    ||    ||   ","blue",attrs=['bold']))
+print(colored("        VV     vv    vv    vv    ","blue",attrs=['bold']))
+print(colored("~~~~~~~~CHECKING STATUS CODE~~~~~~~~~","red",attrs=['bold']))
 
-os.system('touch url_status.txt')
+filex=open('url_status.txt','w+')
 
 port = open('subdomain.txt','r')
 port0 = port.read()
@@ -58,14 +77,34 @@ for p in port1:
 	file5 = open('url_status.txt','a')
 	file5.writelines(p+" = "+status+"\n")
 
-print("+++++++++++++++++++++++++++++++++")
-print("        ||        ")
-print("        VV        ")
-print('status code is done wait for nmap scan results')
+print("\n")
+print(colored("+++++++++++++++++++++++++++++++++++++","blue",attrs=['bold']))
+print(colored("        ||     ||    ||    ||   ","blue",attrs=['bold']))
+print(colored("        VV     vv    vv    vv    ","blue",attrs=['bold']))
+print(colored("~~~~~~~~STATUS CODE IS DONE WORKING ON NSLOOKUP~~~~~~~~~","red",attrs=['bold']))
 
 
-subprocess.check_output("mkdir nmapp",shell=True)
+subprocess.call("nslookup -type=any > nslookup.txt {}".format(x.d), shell=True)
 
-subprocess.check_output("cd nmapp",shell=True)
+print("\n")
+print(colored("+++++++++++++++++++++++++++++++++++++","blue",attrs=['bold']))
+print(colored("        ||     ||    ||    ||   ","blue",attrs=['bold']))
+print(colored("        VV     vv    vv    vv    ","blue",attrs=['bold']))
+print(colored("~~~~~~~~NSLOOKUP DONE WORKING ON WHOIS~~~~~~~~~","red",attrs=['bold']))
 
-subprocess.call("nmap -sC -sV -oA ./nmapp/output {}".format(x.d), shell=True)
+subprocess.call("whois > whois.txt {}".format(x.d), shell=True)
+
+print("\n")
+print(colored("+++++++++++++++++++++++++++++++++++++","blue",attrs=['bold']))
+print(colored("        ||     ||    ||    ||   ","blue",attrs=['bold']))
+print(colored("        VV     vv    vv    vv    ","blue",attrs=['bold']))
+print(colored("~~~~~~~~WHOIS DONE GO FORWARD WITH NMAP SCAN~~~~~~~~~","red",attrs=['bold']))
+
+subprocess.check_output("mkdir nmap",shell=True)
+
+subprocess.check_output("cd nmap",shell=True)
+
+subprocess.call("nmap -sC -sV -oA ./nmap/output {}".format(x.d), shell=True)
+
+print("\n")
+print(colored("~~~~~~~~~~BASIC RECON IS COMPLETED~~~~~~~~~~","red",attrs=['bold']))
