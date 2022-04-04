19301d398114a056283f12a8fa4c4102cea5a568
felicanx <96733551+felicanx@users.noreply.github.com>
2022-04-04 22:47:59 +0530

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
 
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install nmap
+sudo apt-get install whois
+sudo apt-get install dnsutils
+pip3 install requests
+pip3 install argparse
